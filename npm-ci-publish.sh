65610e245c4f19a3712540adac062ab99190ac48
Julia Samol <j.samol@papers.ch>
2023-07-27 11:59:36 +0200

    feat(ci): set up pipeline

---
+++ npm-ci-publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
+
+git update-index --assume-unchanged npm-ci-publish.sh
+git update-index --assume-unchanged npm-ci-publish-beta-only.sh
+
+git --no-pager diff
+
+VERSION=$(node -pe 'JSON.parse(process.argv[1]).version.indexOf("beta")' "$(cat lerna.json)")
+
+if [ "$VERSION" = "-1" ]
+then
+  yarn lerna publish from-package --contents dist --yes
+else
+  echo "version is beta, using --dist-tag next"
+  yarn lerna publish from-package --contents dist --dist-tag next --yes
+fi
+
+rm .npmrc
